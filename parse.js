d320b55b8dc060d8cfccb05adf2a2decac4f8179
Wangki Hong <leejg8130@naver.com>
2023-11-25 20:44:18 +0900

    wangki first commit pares

---
+++ parse.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+// 파일을 읽어오는 함수
+function readDataFromFile(filePath) {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        return data;
+    } catch (err) {
+        console.error('파일을 읽어오는 중 에러 발생:', err);
+        return null;
+    }
+}
+
+// 데이터를 가게 객체로 파싱하는 함수
+function parseData(data) {
+    const lines = data.split('\n');
+    const stores = [];
+
+    lines.forEach(line => {
+        const [name, location, genre, foodType, priceInfo, costPerformance] = line.split('\t');
+        const prices = priceInfo.split(/\s+/).filter(Boolean);
+
+        const store = {
+            name,          // 식당 이름
+            location,      // 위치
+            genre,         // 장르
+            foodType,      // 음식 종류
+            price: prices[0],  // 가격
+            costPerformance: costPerformance.trim(),   // \r 제거
+        };
+
+        stores.push(store);
+    });
+
+    return stores;
+}
+
+// 데이터 파일 경로
+const filePath = 'data.txt';
+
+// 파일에서 데이터 읽기
+const rawData = readDataFromFile(filePath);
+
+if (rawData) {
+    // 데이터 파싱
+    const parsedData = parseData(rawData);
+
+    // 파싱된 데이터 출력
+    console.log(parsedData);
+}
